598b928b706b73b7e11f374cfb3dcf9e5cb370f8
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-12-05 23:14:25 +0900

    init

---
+++ viewer/js/settings.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export const SCALE = 1;
+export const MARGIN = 1; // >= 1
+export const PITCH = (MARGIN + 1) * 2;
+export const COLOR_SET = {rough: 0xffffff, smooth: 0x1e90ff, module: 0xffefd5, pin: 0xfff095};
+export const DEFAULT_COLOR = 0xffffff;
+export const DEFAULT_TRANSPARENT = true;
+export const DEFAULT_OPACITY = 0.3;
