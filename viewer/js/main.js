a7e3a9f297213b171ee528c9b7c32d661c51b7ff
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-12-10 02:57:53 +0900

    D&D実装

--- viewer/js/main.js
+++ viewer/js/main.js
@@ -1,33 +1,33 @@
 "use strict";
 
 var main = function(data) {
-  var scene = new THREE.Scene();
+  let scene = new THREE.Scene();
 
-  var width  = window.innerWidth;
-  var height = window.innerHeight;
-  var fov    = 60;
-  var aspect = width / height;
-  var near   = 1;
-  var far    = 1000;
-  var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  let width  = window.innerWidth;
+  let height = window.innerHeight;
+  let fov    = 60;
+  let aspect = width / height;
+  let near   = 1;
+  let far    = 1000;
+  let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(0, -40, 35);
 
-  var renderer = new THREE.WebGLRenderer();
+  let renderer = new THREE.WebGLRenderer();
   renderer.setSize(width, height);
   renderer.setClearColor(new THREE.Color(0xffffff));
   document.getElementById('canvas').appendChild(renderer.domElement);
 
-  var directionalLight = new THREE.DirectionalLight(0xffffff, settings.DIRECTIONAL_LIGHT_LEVEL);
-  var ambientLight = new THREE.AmbientLight(0xffffff, settings.AMBIENT_LIGHT_LEVEL);
+  let directionalLight = new THREE.DirectionalLight(0xffffff, settings.DIRECTIONAL_LIGHT_LEVEL);
+  let ambientLight = new THREE.AmbientLight(0xffffff, settings.AMBIENT_LIGHT_LEVEL);
   directionalLight.position.set(0, -0.5, 0.7);
   scene.add(directionalLight);
   scene.add(ambientLight);
 
-  var circuit = CircuitCreator.create(data);
+  let circuit = CircuitCreator.create(data);
 
   CircuitDrawer.draw(circuit, scene);
 
-  var controls = new THREE.OrbitControls(camera);
+  let controls = new THREE.OrbitControls(camera);
 
   (function renderLoop() {
     requestAnimationFrame(renderLoop);
@@ -36,18 +36,64 @@ var main = function(data) {
   })();
 };
 
-//window.addEventListener('DOMContentLoaded', main, false);
+$(function() {
+  let dropZone = $('#drop-zone');
+  let fileSelector = $('#file-selector');
 
-function handleFileSelect() {
-  var file = document.getElementById('file').files[0];
-  var reader = new FileReader();
+  // イベントをキャンセル
+  let cancelEvent = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
 
-  reader.onload = function(evt) {
-    document.getElementById('input').style.display = 'none';
-    let json = evt.target.result;
-    let data = JSON.parse(json);
-    main(data);
-  };
+  // dragenter, dragover イベントのデフォルト処理をキャンセル
+  dropZone.on('dragenter', cancelEvent);
+  dropZone.on('dragover', cancelEvent);
 
-  reader.readAsText(file, 'utf-8');
-}
+  // イベントをファイル選択ダイアログの表示に変更
+  let openFileSelectionDialog = function(event) {
+    fileSelector.click();
+    return cancelEvent(event);
+  }
+
+  // click イベントをファイル選択ダイアログの表示に変更
+  dropZone.on('click', openFileSelectionDialog);
+
+  let readFile = function(file) {
+    // ファイルの内容は FileReader で読み込む
+    let fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      // event.target.result に読み込んだファイルの内容が入る
+      dropZone.hide();
+      fileSelector.hide();
+      let json = event.target.result;
+      let data = JSON.parse(json);
+      main(data);
+    }
+    fileReader.readAsText(file);
+  }
+
+  // ドロップ時のイベントハンドラの設定
+  let handleDroppedFile = function(event) {
+    let file = event.originalEvent.dataTransfer.files[0];
+    readFile(file);
+    // デフォルトの処理をキャンセル
+    cancelEvent(event);
+    return false;
+  }
+
+  // 選択時のイベントハンドラの設定
+  let handleSelectedFile = function(event) {
+    if(event.target.value === "") return false;
+    let file = event.target.files[0];
+    readFile(file);
+    // デフォルトの処理をキャンセル
+    cancelEvent(event);
+    return false;
+  }
+
+  // ドロップ時のイベントハンドラの設定
+  dropZone.on('drop', handleDroppedFile);
+  fileSelector.on('change', handleSelectedFile);
+});
